d0a1df8fa213e08f893c0386f94055b2de93b6ec
Alexander M <aleex1742@gmail.com>
2022-10-16 15:29:45 -0600

    resizing fixes

--- app/src/main/java/montanez/alexander/saturnwallpapers/utils/Extensions.kt
+++ app/src/main/java/montanez/alexander/saturnwallpapers/utils/Extensions.kt
@@ -46,30 +46,31 @@ fun Date.isTheSameDayAs(date: Date) : Boolean{
 }
 
 fun Bitmap.getWithFixedSize() : Bitmap{
-    val maxSize = 8000000 //10MB
-    val actualSize = this.allocationByteCount
-    val bitmap = if(actualSize <= maxSize) this
-    else{
-        val quality = ((maxSize.toFloat()/actualSize.toFloat()) * 100).toInt()
-        val output = ByteArrayOutputStream()
-        this.compress(Bitmap.CompressFormat.JPEG,quality,output)
-        BitmapFactory.decodeStream(ByteArrayInputStream(output.toByteArray()))
-    }
-
-    val maxW = 1080
-    val maxH = 2280
-    var w = bitmap.width
-    var h = bitmap.height
+    val maxW = 1080 * 2
+    val maxH = 2280 * 2
+    var w = this.width
+    var h = this.height
     if(w == h && w >= maxW && h >= maxH) {
         w = maxH
         h = maxH
     } else if (w > h && w > maxW){
-        w = maxW
         h = (h*maxW)/w
+        w = maxW
     } else if (h > w && h > maxW){
         w = (w*maxH)/h
         h = maxH
     }
 
-    return Bitmap.createScaledBitmap(bitmap,w,h,true)
+    val scaledBitmap = Bitmap.createScaledBitmap(this,w,h,true)
+
+    val maxSize = 8000000 //10MB
+    val actualSize = scaledBitmap.allocationByteCount
+
+    return if(actualSize <= maxSize) scaledBitmap
+    else{
+        val quality = ((maxSize.toFloat()/actualSize.toFloat()) * 100).toInt()
+        val output = ByteArrayOutputStream()
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG,quality,output)
+        BitmapFactory.decodeStream(ByteArrayInputStream(output.toByteArray()))
+    }
 }
